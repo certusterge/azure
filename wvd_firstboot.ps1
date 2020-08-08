77e54505cadfc1a0f106029e3322c11f25075ae6
certusterge <69392095+certusterge@users.noreply.github.com>
2020-08-08 15:58:03 +0100

    Update wvd_firstboot.ps1

--- wvd_firstboot.ps1
+++ wvd_firstboot.ps1
@@ -11,14 +11,24 @@
 ######################################
 
 #variables
-$regionalsettingsURL = "https://raw.githubusercontent.com/certusterge/azure/master/wvd_firstboot_regionalsettings.xml"
-$RegionalSettings = "D:\WVDFirstBoot.xml"
+#$regionalsettingsURL = "https://raw.githubusercontent.com/certusterge/azure/master/wvd_firstboot_regionalsettings.xml"
+$RegionalSettings = "D:\wvd_firstboot_regionalsettings.xml"
+$RegionalSettingsSource = "\\aquagroup.cloud\SysVol\aquagroup.cloud\Policies\{2E400681-C2BB-4228-BCDC-5291BBC4275F}\Machine\Scripts\Startup\wvd_firstboot_regionalsettings.xml"
+$RunOnceFile = "C:\GPO\regionalsettingsapplied"
+$FileExists	= Test-Path $RunOnceFile
 
 
 #downdload regional settings file
-$webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+#$webclient = New-Object System.Net.WebClient
+#$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 
+#copy file to local temp drive
+
+Copy-Item -Path $RegionalSettingsSource -Destination $RegionalSettings
+
+
+#checkfiles
+If ($FileExists -eq $False) {
 
 # Set Locale, language etc. 
 & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
@@ -30,9 +40,13 @@ Set-Culture -CultureInfo en-GB
 Set-WinHomeLocation -GeoId 242
 Set-TimeZone -Name "GMT Standard Time"
 
+#create runoncefile
+New-Item -ItemType file $RunOnceFile
+
 #gpupdate
-Invoke-GPUpdate
+gpupdate
 
 # restart virtual machine after delay
-Start-sleep -Seconds 300
+Start-sleep -Seconds 60
 Restart-Computer
+}
